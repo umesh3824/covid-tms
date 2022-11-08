60e352f13a42a4fb7b294a964a51e299de4665a4
Umesh Chaudhari <55888684+umesh3824@users.noreply.github.com>
2022-11-08 20:50:48 +0530

    db

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,9 @@
 //time zone
 date_default_timezone_set('Asia/Kolkata');
 //database connection
-$con=mysqli_connect("localhost","root","","covidtmsdb");
+$con=mysqli_connect("localhost","u799378581_covid","Covid@123","u799378581_covidtmsdb");
+
+// $con=mysqli_connect("localhost","root","","covidtmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
